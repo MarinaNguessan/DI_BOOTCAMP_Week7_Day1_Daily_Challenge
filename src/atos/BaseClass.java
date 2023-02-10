4adaa13348a4935f5bed09dd271392752d6f63fa
MarinaNguessan <marinanguessan23@gmail.com>
2023-02-10 16:14:51 +0000

    first commit

---
+++ src/atos/BaseClass.java
@@ -0,0 +1,14 @@
+package atos;
+
+public class BaseClass {
+	
+	public void display() {
+        System.out.println("Méthode de la classe de base");
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/atos/DerivedClass.java
@@ -0,0 +1,13 @@
+package atos;
+
+public class DerivedClass extends BaseClass{
+	
+	
+	public static void main(String[] args) {
+		
+		 DerivedClass derivedObject = new DerivedClass();
+	        derivedObject.display();
+
+	}
+
+}
